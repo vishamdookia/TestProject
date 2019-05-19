4fb3a8475472a3b18df617d71d3ae4dca0867101
SVS <SVS@L-PC01NW5B>
2019-05-19 05:58:39 +0400

    my first git commit

---
+++ TestGitRepo/src/javaTestGit/JavaTest.java
@@ -0,0 +1,11 @@
+package javaTestGit;
+
+public class JavaTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("If you are seeing this msg, this means you have successfully created git");
+
+	}
+
+}
